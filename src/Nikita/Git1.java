0804fe4c92a77b425ede7d8d87f9f3617d2e7659
Nikodimovs <nikita.nikodimovs@sk.lvt.lv>
2025-09-02 11:42:16 +0300

    skaitla uzmesana ir realizerta

--- src/Nikita/Git1.java
+++ src/Nikita/Git1.java
@@ -1,10 +1,15 @@
 package Nikita;
 
+import java.util.Random;
+
 public class Git1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+int skaitlis;
+Random rand = new Random();
+skaitlis = rand.nextInt(6)+1;
+System.out.println("Uzkrita skatilis: "+ skaitlis);
 	}
 
 }
+
