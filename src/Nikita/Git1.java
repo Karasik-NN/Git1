59f3066ac7f053a3fa3e13fca00ee508de609bb0
Nikodimovs <nikita.nikodimovs@sk.lvt.lv>
2025-09-02 12:05:52 +0300

    add cik reizes mest kaulinu

--- src/Nikita/Git1.java
+++ src/Nikita/Git1.java
@@ -3,14 +3,17 @@ package Nikita;
 import java.util.Random;
 
 public class Git1 {
-static void mestKaulinu() {
+static void mestKaulinu(int reizes) {
 	int skaitlis;
 Random rand = new Random();
-skaitlis = rand.nextInt(6)+1;
+for (int i=1; i<reizes; i++) {
+	skaitlis = rand.nextInt(6)+1;
 System.out.println("Uzkrita skatilis: "+ skaitlis);
 }
+
+}
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(5);
 
 
 	}
